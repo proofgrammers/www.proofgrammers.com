13dea6d7363cf0bc587461bf24559bbc38835107
benedekaibas <kaibas.benedek02@gmail.com>
2025-09-04 20:51:35 -0400

    style: finished creating AST based on example codes

--- temp/create_ast.py
+++ temp/create_ast.py
@@ -1,7 +1,7 @@
 """Creating AST from the main.py file."""
 import ast
 
-def createAST():
+def create_ast_main():
     """Create an AST of the main.py file."""
     with open("main.py", "r", encoding='utf-8') as fn:
         read_fn = fn.read()
@@ -10,6 +10,34 @@ def createAST():
 
     return ast_file
 
+
+def create_ast_nomain():
+    """Create an AST of the no_main.py file."""
+    with open("no_main.py", "r", encoding='utf-8') as fn:
+        read_file = fn.read()
+        fn_ast = ast.parse(read_file)
+        store_ast = ast.dump(fn_ast, indent=4)
+
+    return store_ast
+
+
+def store_ast():
+    """Storing the AST file that were created for the main.py file."""
+    ast_main = create_ast_main()
+
+    # TODO: Add exception handling if the file does not exist
+    with open("ast.txt", "w", encoding='utf-8') as fn:
+        return fn.write(ast_main)
+
+def store_no_main_ast():
+    """Storing the AST file that were created for the no_main.py file."""
+    ast_no_main = create_ast_nomain()
+
+    # TODO: Add exception handling if the file does not exist
+    with open("no_main_ast.txt", "w", encoding='utf-8') as fn:
+        return fn.write(ast_no_main)
+
+
 if __name__ == "__main__":
-    object = createAST()
-    print(object)
+    store_ast()
+    store_no_main_ast()
