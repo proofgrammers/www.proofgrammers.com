08714a87b0e4e7aed56a2bb8275f95e17a9c28b3
benedekaibas <kaibas.benedek02@gmail.com>
2025-09-06 15:25:10 -0400

    finished load_source() function

--- temp/node_ast.py
+++ temp/node_ast.py
@@ -5,3 +5,16 @@ class VisitNode:
         self.code = None
         self.tree = None
         self.source_path = source_path
+
+    def load_source(self, path=None):
+        """Loading Python source codes."""
+        if path:
+            print(f"Path could not be found: {path}")
+        
+        if self.source_path:
+            with open("main.py", "r", encoding='utf-8') as fn:
+                read_file = fn.read()
+                self.code = read_file
+
+            return self.code
+
