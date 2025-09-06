0bf8b8c3ccbd7816656ee94368b4b10ffc07e463
benedekaibas <kaibas.benedek02@gmail.com>
2025-09-06 14:36:25 -0400

    bug: NameError not yet resolved

--- temp/node_ast.py
+++ temp/node_ast.py
@@ -5,23 +5,27 @@ class VisitNode:
         self.code = code
         self.tree = tree
 
-    def createAST(self):
+    def create_ast(self):
         """Creating an AST."""
         with open("main.py", "r", encoding='utf-8') as fn:
             self.code = fn.read()
             self.tree = ast.parse(self.code)
         return self.tree
 
-    def walkNode(self):
+    def walk_node(self, tree):
         """Walking through the nodes of the created AST."""
         ast_tree = self.tree
-
-        for node in ast.walk(ast_tree):
+        ast_nodes = ast.walk(ast_tree)
+    
+        for node in ast_nodes:
             if isinstance(node, ast.FunctionDef):
                 print(f"Detected function definition: {node.name}")
                 
 
 
 if __name__ == "__main__":
-    return_object = VisitNode
-    return_object.walkNode
+    with open("main.py", "r", encoding='utf-8') as fn:
+        code = fn.read()
+        tree = ast.parse(code)
+
+    VisitNode.walk_node(self, tree)
