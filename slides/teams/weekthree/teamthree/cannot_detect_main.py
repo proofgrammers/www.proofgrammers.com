0d4d09c6e5aa232aa18a575042b3c99993377074
benedekaibas <kaibas.benedek02@gmail.com>
2025-09-07 19:35:18 -0400

    bug: return statement moved outside of the loop for avoiding early returns

--- slides/teams/weekthree/teamthree/cannot_detect_main.py
+++ slides/teams/weekthree/teamthree/cannot_detect_main.py
@@ -1,5 +1,24 @@
 """This file contains a main function that our AST tool cannot detect."""
+from typing import Tuple
 
-def main():
-    """Main function that cannot be detected."""
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers."""
+    return a * b
+
+def divide(a: int, b: int) -> int:
+    """Divide two numbers."""
+    try:
+        result = int(a / b)
+        return result
+    except ZeroDivisionError:
+        b_int = int(b)
+        print(f"Cannot divide with {b_int}.")
+
+    return 0
+
+def main(func1, func2, *args, **kwargs):
+    return func1, func2
+
+print(multiply(5, 4))
+print(divide(10, 2))
 
