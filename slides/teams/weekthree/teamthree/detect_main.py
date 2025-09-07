e6c9e454d42bbcbf5c03aff1df5e52475a2e75b7
benedekaibas <kaibas.benedek02@gmail.com>
2025-09-07 15:30:16 -0400

    style: new slide presentation code

---
+++ slides/teams/weekthree/teamthree/detect_main.py
@@ -0,0 +1,46 @@
+import ast
+
+class ASTAnalyzer:
+    def create_ast(self):
+        """Creating an Abstract Syntax Tree."""
+       code_with_main = """def main(word: str) -> str:
+
+    text = "gators"
+
+    if word in text:
+        return "True"
+    else:
+        return "False"
+
+if __name__ == "__main__":
+    print(main("gators"))"""
+
+    def walk_tree(self):
+        """Walking through the created AST."""
+        code = self.create_ast()
+        if code is None:
+            return None
+        functions = []
+        for node in ast.walk(code):
+            if isinstance(node, ast.FunctionDef):
+                functions.append(node.name)
+                return functions
+
+    def find_main(self):
+        """Find main function in nodes created from the AST."""
+        ast_function = self.walk_tree()
+        if not ast_function:
+            return
+        main_name = "'main'"
+
+        for name in ast_function:
+            if name == "main":
+                print(f"Main function found: {name}")
+            else:
+                print("The function you have entered as an input contains no main function.")
+
+
+if __name__ == "__main__":
+    analyzer = ASTAnalyzer()
+    analyzer.walk_tree()
+    analyzer.find_main()
