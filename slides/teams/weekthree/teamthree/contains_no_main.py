ea25d44783d3b816c57b8832e3b640a43d52d9f1
Benedek Kaibas <82393336+benedekaibas@users.noreply.github.com>
2025-09-07 15:50:14 -0400

    Remove unused findWord function and retain no_main

--- slides/teams/weekthree/teamthree/contains_no_main.py
+++ slides/teams/weekthree/teamthree/contains_no_main.py
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+"""In this Python file we create do not create a main function, so we can use it for our main function detecting tool."""
+
 def no_main(word: str) -> int:
     """Count the number of vowels in a word."""
     vowels = "aeiou"
@@ -6,18 +7,3 @@ def no_main(word: str) -> int:
     return count
 
 print(no_main("Gators"))
-=======
-"""In this Python file we create do not create a main function, so we can use it for our main function detecting tool."""
-
-def findWord(word: str) -> str:
-    """Finding word in a string"""
-
-    text = "gators"
-
-    if word in text:
-        return "True"
-    else:
-        return "False"
-
-print(findWord("gators"))
->>>>>>> eee0d38461d7577b5f11fef886d82720ecb11d2f
