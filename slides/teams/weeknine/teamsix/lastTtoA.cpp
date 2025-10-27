f053c8d6f9c7b0251850679768a50322bbab3d3f
benedekaibas <kaibas.benedek02@gmail.com>
2025-10-26 20:31:17 -0400

    edit: added code for cpp implementation

--- slides/teams/weeknine/teamsix/lastTtoA.cpp
+++ slides/teams/weeknine/teamsix/lastTtoA.cpp
@@ -3,12 +3,30 @@
 #include <cctype>
 
 std::string lower(std::string input) {
-    std::string string_lower;
-    std::transform(input.begin(), input.end(), string_lower.begin(),
+    std::transform(input.begin(), input.end(), input.begin(),
                 [](unsigned char character){return std::tolower(character);});
-    return string_lower;
+    return input;
 };
 
+std::string lower_text = lower("ACTTGA");
+
 void lastTtoA() {
-    std::string input = lower;
+    std::string& input_string = lower_text;
+    std::reverse(input_string.begin(), input_string.end()); // string reverse so first element is the last
+    char const& replace_letter = 't';
+    char const& replace_with = 'a';
+
+    std::size_t position = input_string.find(replace_letter);
+    if(position == std::string::npos) return;
+    std::replace(input_string.begin(), input_string.end(), replace_letter, replace_with);
+    
+    std::string normal_string;
+    for (int i = input_string.length() - 1; i>= 0; --i) {
+        normal_string += input_string[i];
+    }
+    std::cout << normal_string;
+}
+
+int main() {
+    lastTtoA();
 }
